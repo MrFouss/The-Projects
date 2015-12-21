638010e9556847a61a8350bc3c0b8f7380961449
th0re <stephane.perrez@gmail.com>
2015-12-21 17:17:58 +0100

    Building class Completed.

--- view/Building.java
+++ view/Building.java
@@ -9,35 +9,43 @@ import java.util.Arrays;
 
 
 /**
- * TODO complete
+ * The main building, defining their shape and color
  */
 public class Building {
 
     private Shape shape;
     private Color color;
 
-    public Building(Color color, Shape ... s) {
+    /**
+     * Constructor of a Building
+     * @param color the color of the building
+     * @param shapes the different shapes composing the building
+     */
+    public Building(Color color, Shape ... shapes) {
 
-        if (s.length > 1) {
-            shape = Arrays.asList(s).stream().reduce(Shape::union).get();
+        if (shapes.length > 1) {
+            shape = Arrays.asList(shapes).stream().reduce(Shape::union).get();
         }else{
-            shape = s[0];
+            shape = shapes[0];
         }
-        setColor(color);
+        this.color = color;
+        shape.setFill(new RadialGradient(0, 0, .5, .5, .8, true, CycleMethod.NO_CYCLE, new Stop(0, color), new Stop(1, color.deriveColor(0,1,.5,1))));
+        shape.setStroke(color.deriveColor(0,1,.5,1));
         shape.setStrokeWidth(3);
     }
 
-
-    public void setColor(Color c) {
-        color = c;
-        shape.setFill(new RadialGradient(0, 0, .5, .5, .8, true, CycleMethod.NO_CYCLE, new Stop(0, color), new Stop(1, color.deriveColor(0,1,.5,1))));
-        shape.setStroke(color.deriveColor(0,1,.5,1));
-    }
-
+    /**
+     * Getter for the color of the building
+     * @return the Color of the Building
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Getter of the Shape of the building
+     * @return the Shape of the Building
+     */
     public Shape getShape() {
         return shape;
     }
